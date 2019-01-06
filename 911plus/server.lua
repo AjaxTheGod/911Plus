effd83ad5d2dee1411b463a34020f428f7afaea4
JHodgson1 <hodgsonjack@hotmail.com>
2019-01-06 18:21:33 +0000

    don't use chatMessage for commands, use RegisterCommand()

--- 911plus/server.lua
+++ 911plus/server.lua
@@ -3,38 +3,20 @@ local webhook = "" --modify, also set in your gateway config!
 --Change the webhook address to your discord webhook address!
 --end config
 
-RegisterServerEvent('notify')
+function process(statusCode, text, headers) end
 
-AddEventHandler('chatMessage', function(source, name, msg)
-	if string.sub(msg, 1, 5) == "/911 " then
-		CancelEvent()
-		local len = string.len(msg)
-		local msg2 = string.sub(msg, 6, len)
-		
-		local wh_content = "**New 911 Call!** ("..name..")\n\n"..msg2
-		
-		PerformHttpRequest(webhook, process, "POST", "content="..wh_content.."&tts=true")
-		local msg = "Dispatcher Notified: "..msg2
-		
-		function process(statusCode, text, headers)
-			--done
-		end
-		
-		TriggerClientEvent("notify", source, msg)
-	elseif string.sub(msg, 1, 7) == "/panic " then
-		CancelEvent()
-		local len = string.len(msg)
-		local msg2 = string.sub(msg, 8, len)
-		
-		local wh_content = "**Panic Button Pressed!** ("..name..")\n\nLocation: "..msg2
-		
-		PerformHttpRequest(webhook, process, "POST", "content="..wh_content.."&tts=true")
-		local msg = "Panic Button Pressed"
-		
-		function process(statusCode, text, headers)
-			--done
-		end
-		
-		TriggerClientEvent("notify", source, msg)
-	end
+RegisterCommand('911', function(source, args, raw)	
+	local wh_content = "**New 911 Call!** ("..GetPlayerName(source)..")\n\n"..table.concat(args, " ")
+	
+	PerformHttpRequest(webhook, process, "POST", "content="..wh_content.."&tts=true")
+	local msg = "Dispatcher Notified: "..table.concat(args, " ")		
+	TriggerClientEvent("notify", source, msg)
+end)
+
+RegisterCommand('panic', function(source, args, raw)		
+	local wh_content = "**Panic Button Pressed!** ("..GetPlayerName(source)..")\n\nLocation: "..table.concat(args, " ")
+	
+	PerformHttpRequest(webhook, process, "POST", "content="..wh_content.."&tts=true")
+	local msg = "Panic Button Pressed"	
+	TriggerClientEvent("notify", source, msg)
 end)
